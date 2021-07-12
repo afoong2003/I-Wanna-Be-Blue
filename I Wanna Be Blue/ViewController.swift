09d952e5a8e34ec3f6cbaf556c5457ec0760d68c
afoong2003 <afoong2003@gmail.com>
2021-07-11 21:33:52 -0500

    Initial Commit

---
+++ I Wanna Be Blue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Wanna Be Blue
+//
+//  Created by Student30 on 7/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
