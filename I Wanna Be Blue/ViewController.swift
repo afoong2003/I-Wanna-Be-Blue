c242c91b4cd236ca3124957860dce383fdf5ba31
afoong2003 <afoong2003@gmail.com>
2021-07-13 10:07:23 -0500

    BLUE
    BLUE

--- I Wanna Be Blue/ViewController.swift
+++ I Wanna Be Blue/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        view.backgroundColor = .blue
+        labelText.text = "It's not easy being green."
+        labelText.textColor = UIColor.green
+    }
+    
 }
 
